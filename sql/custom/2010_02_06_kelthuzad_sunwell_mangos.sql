4d62335c5cb3d10422248c604640fc912ba3f498
[DEV]kelthuzad <kel_thalas@web.de>
2010-03-15 12:33:17 +0100

    sunwell sql fixes

---
+++ sql/custom/2010_02_06_kelthuzad_sunwell_mangos.sql
@@ -0,0 +1,68 @@
+
+# SUNWELL INSTANCE UPDATE
+
+# mob updates:
+
+# npc 25507 (Sunblade Protector)
+# update damage
+UPDATE `creature_template` SET `dmg_multiplier`=1.0 WHERE `entry`=25507; 
+
+# npc 25372 (Sunblade Scout)
+# update damage
+UPDATE `creature_template` SET `dmg_multiplier`=1.0 WHERE `entry`=25372;
+
+# npc 26101 (Fire Fiend)
+# add spell cast 46551 (fire nova)
+UPDATE `creature_template` SET `AIName`="EventAI" WHERE `entry`=26101;
+DELETE FROM `creature_ai_scripts` WHERE `creature_id`=26101;
+INSERT INTO `creature_ai_scripts` VALUES(2610101,26101,0,0,100,3,0,0,20000,25000,11,46551,0,1,0,0,0,0,0,0,0,0,"Fire fiend - fire nova cast");
+# add loot item 22445 (arcane dust) 
+UPDATE `creature_template` SET `lootid`=26101 WHERE `entry`=26101;
+DELETE FROM `creature_loot_template` WHERE `entry`=26101;
+INSERT INTO `creature_loot_template` VALUES(26101,22445,100,0,2,2,0,0,0); 
+
+# update npc hp&mana values
+# npc 25507 (sunblade protector)
+UPDATE `creature_template` SET `minhealth`=387000, `maxhealth`=387000 WHERE `entry`=25507;
+# npc 26101 (fire fiend)
+UPDATE `creature_template` SET `minhealth`=7800, `maxhealth`=7800,`minmana`=3410,`maxmana`=3410 WHERE `entry`=26101;
+# npc 25363,25370,25371 (sunblade cabalist, sunblade dusk priest, sunblade dawn priest)
+UPDATE `creature_template` SET `minhealth`=137000, `maxhealth`=137000, `minmana`=31600,`maxmana`=31600 WHERE `entry`=25363||`entry`=25370||`entry`=25371;
+# npc 25367 (sunblade arch mage)
+UPDATE `creature_template` SET `minhealth`=137000, `maxhealth`=137000 WHERE `entry`=25367;
+# npc 25369,25368 (sunblade vindicator, sunblade slayer)
+UPDATE `creature_template` SET `minhealth`=171000, `maxhealth`=171000 WHERE `entry`=25369||`entry`=25368;
+# npc 25867 (sunblade dragonhawk)
+UPDATE `creature_template` SET `minhealth`=97600, `maxhealth`=97600 WHERE `entry`=25867;
+# npc 25372 (sunblade scout)
+UPDATE `creature_template` SET `minhealth`=14700, `maxhealth`=14700 WHERE `entry`=25372;
+
+# boss updates:
+
+# fixes for boss_Kalecgos fight
+# add npc 24891 (Kalecgos humanoid)
+INSERT INTO `creature` VALUES(600000,24891,580,1,1,0,0,1702.3,938.0,-74.55,4.93,30000,0,0,828555,169350,0,0);
+# update kalecgos, sathorvarr, kalecgos_humanoid
+UPDATE `creature_template` SET `mechanic_immune_mask` = '1073463287',`minmana`=1690000, `maxmana`=1690000  WHERE `entry` = 24850; 
+UPDATE `creature_template` SET `mechanic_immune_mask` = '1073463287'  WHERE `entry` = 24892;
+UPDATE `creature_template` SET `mechanic_immune_mask` = '1073463287'  WHERE `entry` = 24891;
+# update gameobject 187055 (spectral rift)
+UPDATE gameobject_template SET `type`= '22', `data0`='0', `data3`='1', `data6`='0', `data10`='0' WHERE `entry`=187055;
+
+# fixes for boss_brutallus fight
+# update npc 24882 (Brutallus)
+UPDATE `creature_template` SET `minhealth`=7350000,`maxhealth`=7350000 WHERE `entry`=24882;
+
+# fixes for boss_felmyst fight
+# add npc 25038 (Felmyst)
+INSERT INTO creature VALUES(700000,25038,580,1,1,0,0,1459.34,636.8,19.56,19.95,30000,0,0,4900000,3390,0,0);
+
+# fixes for boss_eredar_twins fight
+# update npc 25165 (Sacrolash)
+UPDATE `creature_template` SET `ScriptName` = "boss_sacrolash", `minhealth`=2018000, `maxhealth`=2018000 WHERE `entry` = 25165;
+# update npc 25166 (Alythess)
+UPDATE `creature_template` SET `ScriptName` = "boss_alythess", `minhealth`=2018000, `maxhealth`=2018000  WHERE `entry` = 25166;
+# update npc 25214 (Shadow Image)
+UPDATE `creature_template` SET `ScriptName` = "npc_shadow_image" WHERE `entry` = 25214;
+# update go 50439 (The Second Gate)
+UPDATE `gameobject` SET `position_x`=1832.81, `position_y`=671.08, `position_z`=50.21, state=1 WHERE `guid`=50439;
---
+++ sql/custom/2010_02_06_kelthuzad_sunwell_scriptdev2.sql
@@ -0,0 +1,33 @@
+# sunwell plateau scriptdev2 db update
+
+# boss_felmyst
+INSERT INTO `script_texts` VALUES(-1930000,"Glory to Kil'jaeden! Death to all who oppose!",NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,1,0,7,"felmyst YELL_AGGRO");
+INSERT INTO `script_texts` VALUES(-1930001,"I am stronger than ever before!",NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,1,0,7,"felmyst YELL_TAKEOFF");
+INSERT INTO `script_texts` VALUES(-1930002,"Choke on your final breath!",NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,1,0,7,"felmyst YELL_BREATH");
+INSERT INTO `script_texts` VALUES(-1930003,"I kill for the master!",NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,1,0,7,"felmyst YELL_SLAY1");
+INSERT INTO `script_texts` VALUES(-1930004,"The end has come!",NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,1,0,7,"felmyst YELL_SLAY2");
+INSERT INTO `script_texts` VALUES(-1930005,"Kil'jaeden... will... prevail...",NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,1,0,7,"felmyst YELL_DEATH");
+INSERT INTO `script_texts` VALUES(-1930006,"No more hesitation! Your fates are written!",NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,1,0,7,"felmyst YELL_BERSERK");
+INSERT INTO `script_texts` VALUES(-1930007,"Madrigosa deserved a far better fate. You did what had to be done, but this battle is far from over.",NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,1,0,7,"felmyst YELL_OUTRO (kalecgos)");
+
+# boss_eredar_twins
+INSERT INTO `script_texts` VALUES(-1940000,"Misery...",NULL,NULL,"Elend...",NULL,NULL,NULL,NULL,NULL,0,1,0,7,"YELL_SACROLASH_AGGRO_1");
+INSERT INTO `script_texts` VALUES(-1940001,"Confusion...",NULL,NULL,"Verwirrung...",NULL,NULL,NULL,NULL,NULL,0,1,0,7,"YELL_SACROLASH_AGGRO_2");
+INSERT INTO `script_texts` VALUES(-1940002,"Mistrust...",NULL,NULL,"Misstrauen...",NULL,NULL,NULL,NULL,NULL,0,1,0,7,"YELL_SACROLASH_AGGRO_3");
+INSERT INTO `script_texts` VALUES(-1940003,"These are the hallmarks...",NULL,NULL,"Dies sind die Kennzeichen...",NULL,NULL,NULL,NULL,NULL,0,1,0,7,"YELL_SACROLASH_AGGRO_4");
+INSERT INTO `script_texts` VALUES(-1940004,"Depravity...",NULL,NULL,"Leid...",NULL,NULL,NULL,NULL,NULL,0,1,0,7,"YELL_ALYTHESS_AGGRO_1");
+INSERT INTO `script_texts` VALUES(-1940005,"Hatred...",NULL,NULL,"Hass...",NULL,NULL,NULL,NULL,NULL,0,1,0,7,"YELL_ALYTHESS_AGGRO_2");
+INSERT INTO `script_texts` VALUES(-1940006,"Chaos...",NULL,NULL,"Chaos...",NULL,NULL,NULL,NULL,NULL,0,1,0,7,"YELL_ALYTHESS_AGGRO_3");
+INSERT INTO `script_texts` VALUES(-1940007,"These are the pillars...",NULL,NULL,"Dies sind die Säulen...",NULL,NULL,NULL,NULL,NULL,0,1,0,7,"YELL_ALYTHESS_AGGRO_4");
+INSERT INTO `script_texts` VALUES(-1940008,"I... fade.",NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,1,0,7,"YELL_SACROLASH_DEATH");
+INSERT INTO `script_texts` VALUES(-1940009,"Alythess! Your fire burns within me!",NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,1,0,7,"YELL_SACROLASH_EMPOWER");
+INSERT INTO `script_texts` VALUES(-1940010,"Shadow to the aid of fire!",NULL,NULL,"Schatten dem Feuer zu Hilfe!",NULL,NULL,NULL,NULL,NULL,0,1,0,7,"YELL_SACROLASH_NOVA");
+INSERT INTO `script_texts` VALUES(-1940011,"Time is a luxury you no longer possess!",NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,1,0,7,"YELL_SACROLASH_BERSERK");
+INSERT INTO `script_texts` VALUES(-1940012,"Shadows, engulf!",NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,1,0,7,"YELL_SACROLASH_KILL_1");
+INSERT INTO `script_texts` VALUES(-1940013,"Ee-nok Kryul!",NULL,NULL,"Ee-nok Kryul!",NULL,NULL,NULL,NULL,NULL,0,1,0,7,"YELL_SACROLASH_KILL_2");
+INSERT INTO `script_texts` VALUES(-1940014,"Fire to the aid of shadow!",NULL,NULL,"Feuer dem Schatten zu Hilfe!",NULL,NULL,NULL,NULL,NULL,0,1,0,7,"YELL_ALYTHESS_CONFLAGRATE");
+INSERT INTO `script_texts` VALUES(-1940015,"Sacrolash!",NULL,NULL,"Sacrolash!",NULL,NULL,NULL,NULL,NULL,0,1,0,7,"YELL_ALYTHESS_EMPOWER");
+INSERT INTO `script_texts` VALUES(-1940016,"Fire, consume!",NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,1,0,7,"YELL_ALYTHESS_KILL_1");
+INSERT INTO `script_texts` VALUES(-1940017,"Ed-ir Halach!",NULL,NULL,"Ed-ir Halach!",NULL,NULL,NULL,NULL,NULL,0,1,0,7,"YELL_ALYTHESS_KILL_2");
+INSERT INTO `script_texts` VALUES(-1940018,"Your luck has run its course!",NULL,NULL,"Euer Glück ist verbraucht!spell_scripts",NULL,NULL,NULL,NULL,NULL,0,1,0,7,"YELL_ALYTHESS_BERSERK");
+INSERT INTO `script_texts` VALUES(-1940019,"Der'ek manul!",NULL,NULL,"Der'ek manul!",NULL,NULL,NULL,NULL,NULL,0,1,0,7,"YELL_ALYTHESS_DEATH");

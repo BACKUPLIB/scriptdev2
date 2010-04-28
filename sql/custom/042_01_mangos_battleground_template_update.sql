7fc2935e00a0e6eba20afeb7425e120d985f5171
fandor <fandor.orangevirus@gmx.at>
2010-04-28 20:47:14 +0200

    [OV42] Reduce MinPlayersPerTeam for battlegrounds
    MinPlayersPerTeam: AV = 12, WS = 5, AB = 7, EY = 7.

---
+++ sql/custom/042_01_mangos_battleground_template_update.sql
@@ -0,0 +1,4 @@
+UPDATE battleground_template SET MinPlayersPerTeam=12 WHERE id=1;
+UPDATE battleground_template SET MinPlayersPerTeam=5 WHERE id=2;
+UPDATE battleground_template SET MinPlayersPerTeam=7 WHERE id=3;
+UPDATE battleground_template SET MinPlayersPerTeam=7 WHERE id=7;

ad90d6ddfec90f5fbccb74966202e6d9b687bbb2
[DEV]kelthuzad <kel_thalas@web.de>
2010-04-28 18:18:13 +0200

    [OV38] fix for mantis bug 2506 by Alphawolf

---
+++ sql/custom/038_01_mangos_inscr_glad.sql
@@ -0,0 +1,3 @@
+# fix for mantis id 2506 by Alphawolf
+INSERT INTO `npc_vendor` (`entry`, `item`, `maxcount`, `incrtime`, `ExtendedCost`) VALUES ('12781', '44957', '0', '0', '165');
+INSERT INTO `npc_vendor` (`entry`, `item`, `maxcount`, `incrtime`, `ExtendedCost`) VALUES ('12793', '44957', '0', '0', '165');

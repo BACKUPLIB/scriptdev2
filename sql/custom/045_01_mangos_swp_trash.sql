4305590d42ad517fbe0c6bb4db6729b408bd7412
[DEV]kelthuzad <kel_thalas@web.de>
2010-04-29 21:25:47 +0200

    [OV47] swp trashmob damage update
    increased dmg of sunblade scout and sunblade protector

---
+++ sql/custom/045_01_mangos_swp_trash.sql
@@ -0,0 +1,3 @@
+# update damage output of swp trashmobs sunblade protector and sunblada scout
+UPDATE `creature_template` SET `mindmg` = 780 ,`maxdmg`=2450, `dmg_multiplier`=5 WHERE `entry`=25507;
+UPDATE `creature_template` SET `dmg_multiplier` = 3 WHERE `entry`=25372;

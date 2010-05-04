b3c6c99085694ce707c1a64b053de813ac5e3450
pasdVn <pasdVn3@gmx.de>
2010-05-04 19:35:26 +0200

    [OV58] Quest fix: 14111
    Kind of "workaround", but infact more sexy!

---
+++ sql/custom/058_01_mangos_quest_template.sql
@@ -0,0 +1,3 @@
+-- Quest 14111 fix: Nich blizzlike, aber eigentlich sogar schöner so!
+UPDATE `quest_template` SET `RewSpell` = '0', `RewSpellCast` = '0', `RewItemId1` = '46978', `RewItemCount1` = '1'
+WHERE `entry` =14111;

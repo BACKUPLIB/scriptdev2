74b1354349b245b7439c0be8e7b5555adde6e32b
[DEV]kelthuzad <kel_thalas@web.de>
2010-05-11 22:00:33 +0200

    [OV67] update za tl4 lootchance
    decreased lootchance to 5%

--- sql/custom/066_01_mangos_creature_loot_template.sql
+++ sql/custom/066_01_mangos_creature_loot_template.sql
@@ -9,4 +9,4 @@ INSERT INTO `creature_loot_template` VALUES (24001,33500,0,1,1,1,0,0,0);
 */
 # set tl4 in za to item 33809 (Amani War Bear)
 DELETE FROM `creature_loot_template` WHERE `entry` = 24001;
-INSERT INTO `creature_loot_template` VALUES (24001,33500,0.20,0,1,1,0,0,0);
+INSERT INTO `creature_loot_template` VALUES (24001,33809,0.05,0,1,1,0,0,0);

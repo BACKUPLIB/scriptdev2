9bbebf50d87d5056188caaddb62c551103e6fa21
[DEV]kelthuzad <kel_thalas@web.de>
2010-06-04 00:54:16 +0200

    [OV103] delete wrong SQLs
    delete some wrong/outdated SQL files

--- sql/custom/075_01_mangos_creature.sql
+++
@@ -1,3 +0,0 @@
-#spawn npc 25003 (mantis bug 2571)
-DELETE FROM `creature` WHERE `id` = 25003;
-INSERT INTO `creature` (`id`,`map`,`spawnMask`,`phaseMask`,`position_x`,`position_y`,`position_z`,`orientation`,`spawntimesecs`,`curhealth`,`curmana`) VALUES (25003,530,1,1,12588.90,-6916.43,4.602,6.12,500,7000,3309);

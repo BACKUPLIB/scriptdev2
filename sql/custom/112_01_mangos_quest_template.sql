a0b9576c104c6d60b5de4937c1b6b77a84424143
[DEV]kelthuzad <kel_thalas@web.de>
2010-06-27 12:02:46 +0200

    [OV112] fix 2 dk quest bugs

---
+++ sql/custom/112_01_mangos_quest_template.sql
@@ -0,0 +1,4 @@
+# remove quest item on quest 12698 end 
+UPDATE `quest_template` SET `ReqItemId1` = 39253 WHERE `entry` = 12698;
+# remove quest item on quest 12720 end
+UPDATE `quest_template` SET `ReqItemId1` = 39371 WHERE `entry` = 12720;

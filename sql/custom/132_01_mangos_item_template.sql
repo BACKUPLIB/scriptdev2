f8d446e81d6263913816541079992bc218fa7086
[DEV]kelthuzad <kel_thalas@web.de>
2010-07-16 17:45:35 +0200

    [OV132] refix mantis bug 2462
    and renamed OV131 fix

--- sql/custom/132_01_mangos_item_template.sql
+++ sql/custom/132_01_mangos_item_template.sql
@@ -1,5 +1,5 @@
 # mantis bug 2462
 # fix item 29109 remove spell 38164 because it proccs to often (should only procc in one mob fight)
-UPDATE `item_template` SET `spellid_2` = 0, `spelltrigger_2` = 0 WHERE `entry` = 29109;
+UPDATE `item_template` SET `spellid_1` = 0, `spelltrigger_1` = 0, WHERE `entry` = 29109;
 # fix item 29108 remove spell 38164 because it proccs to often (should only procc in one mob fight)
-UPDATE `item_template` SET `spellid_2` = 0, `spelltrigger_2` = 0 WHERE `entry` = 29108;
+UPDATE `item_template` SET `spellid_1` = 0, `spelltrigger_1` = 0 WHERE `entry` = 29108;

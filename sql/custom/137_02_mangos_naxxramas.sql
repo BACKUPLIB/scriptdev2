b6cd65d23b6018df940600bc9687d5e465028b72
[DEV]kelthuzad <kel_thalas@web.de>
2010-07-25 16:24:15 +0200

    [OV139] added script for boss grobbulus
    script not testet ingame yet!

--- sql/custom/137_02_mangos_naxxramas.sql
+++
@@ -1,4 +0,0 @@
-# update naxxramas instance
-
-# remove widows embrace from naxxramas worshipper
-DELETE FROM `creature_ai_scripts` WHERE `id` = 1650612;
---
+++ sql/custom/139_01_mangos_naxxramas.sql
@@ -0,0 +1,8 @@
+# update naxxramas instance
+
+# remove widows embrace from naxxramas worshipper AI
+DELETE FROM `creature_ai_scripts` WHERE `id` = 1650612;
+
+# add boss scripts for boss_grobbulus
+UPDATE `creature_template` SET `ScriptName` = "boss_grobbulus" WHERE `entry` = 15931;
+UPDATE `creature_template` SET `ScriptName` = "npc_grobbulus_poison_cloud" WHERE `entry` = 16363;

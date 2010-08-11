a5f3592d950c552dd2f63579f918dad8e38467c2
pasdVn <pasdVn3@gmx.de>
2010-08-11 12:33:18 +0200

    [OV158] Added quest gossip option for pet-trainer menu.
    Mantis bug 2730.

---
+++ sql/custom/158_01_mangos_gossip.sql
@@ -0,0 +1,3 @@
+-- Quest giver gossip for pet trainer gossip menu (4783)
+INSERT INTO `gossip_menu_option` (`menu_id`, `id`, `option_icon`, `option_text`, `option_id`, `npc_option_npcflag`, `action_menu_id`, `action_poi_id`, `action_script_id`, `box_coded`, `box_money`, `box_text`, `cond_1`, `cond_1_val_1`, `cond_1_val_2`, `cond_2`, `cond_2_val_1`, `cond_2_val_2`, `cond_3`, `cond_3_val_1`, `cond_3_val_2`) VALUES
+(4783, 1, 0, 'GOSSIP_OPTION_QUESTGIVER', 2, 2, 0, 0, 0, 0, 0, NULL, 0, 0, 0, 0, 0, 0, 0, 0, 0);

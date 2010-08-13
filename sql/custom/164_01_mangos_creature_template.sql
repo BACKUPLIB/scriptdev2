0066ec7b441b9e4aaea5db13f6f44a96201b8029
Fandor <erwin.holzhauser@gmx.at>
2010-08-13 17:44:38 +0200

    [OV164] small improvement for quest 11322

---
+++ sql/custom/164_01_mangos_creature_template.sql
@@ -0,0 +1,1 @@
+UPDATE creature_template SET minlevel=70, maxlevel=70, minhealth=8980, maxhealth=8980, mindmg=312, maxdmg=413, dmg_multiplier=2.5, attackpower=155, baseattacktime=1500 WHERE entrY=27959 

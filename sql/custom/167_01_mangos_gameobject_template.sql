df54d856ea0437e512b60d58ec75b81a1794eeca
pasdVn <pasdVn3@gmx.de>
2010-08-18 00:05:46 +0200

    [OV167] Restored original gameobject_template of "Eye of Acherus Control Mechanism"
    ...whyever this has been changed on the servers already...

---
+++ sql/custom/167_01_mangos_gameobject_template.sql
@@ -0,0 +1,3 @@
+/*Eye of Acherus Control Mechanism*/
+REPLACE INTO `gameobject_template` (`entry`, `type`, `displayId`, `name`, `IconName`, `castBarCaption`, `unk1`, `faction`, `flags`, `size`, `questItem1`, `questItem2`, `questItem3`, `questItem4`, `questItem5`, `questItem6`, `data0`, `data1`, `data2`, `data3`, `data4`, `data5`, `data6`, `data7`, `data8`, `data9`, `data10`, `data11`, `data12`, `data13`, `data14`, `data15`, `data16`, `data17`, `data18`, `data19`, `data20`, `data21`, `data22`, `data23`, `mingold`, `maxgold`, `ScriptName`) VALUES
+(191609, 10, 8123, 'Kontrollmechanismus des Auges von Acherus', '', '', '', 0, 6553636, 1, 0, 0, 0, 0, 0, 0, 1635, 12641, 0, 0, 0, 0, 0, 0, 0, 0, 51888, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '');

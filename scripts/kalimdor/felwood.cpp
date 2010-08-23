7041bab4745b347ea5ab752cb95bd3e89c7be86e
deverend <deverend@5f9c896b-1e26-0410-94da-f77f675e2462>
2010-08-23 17:20:59 +0000

    Added support for quest 7603. Patch by Schmoozerd. Fixed compile after r1798. Fixed sql files from r1791.
    git-svn-id: https://scriptdev2.svn.sourceforge.net/svnroot/scriptdev2@1800 5f9c896b-1e26-0410-94da-f77f675e2462

--- scripts/kalimdor/felwood.cpp
+++ scripts/kalimdor/felwood.cpp
@@ -17,7 +17,7 @@
 /* ScriptData
 SDName: Felwood
 SD%Complete: 95
-SDComment: Quest support: related to 4101/4102 (To obtain Cenarion Beacon), 4506, 7603 (Summon Pollo Grande)
+SDComment: Quest support: related to 4101/4102 (To obtain Cenarion Beacon), 4506, 7603, 7603 (Summon Pollo Grande)
 SDCategory: Felwood
 EndScriptData */
 
@@ -25,6 +25,7 @@ EndScriptData */
 npc_kitten
 npcs_riverbreeze_and_silversky
 npc_niby_the_almighty
+npc_kroshius
 EndContentData */
 
 #include "precompiled.h"
@@ -331,92 +332,135 @@ bool ChooseReward_npc_niby_the_almighty(Player* pPlayer, Creature* pCreature, co
     return true;
 }
 
-/*###### 
-## npc_kroshius 
-######*/ 
-enum { 
-	    NPC_KROSHIUS        = 14467, 
-	    SPELL_KNOCKBACK     = 10101, 
-	    SAY_KROSHIUS_REVIVE = -1022000 
-}; 
+/*######
+## npc_kroshius
+######*/
 
-struct MANGOS_DLL_DECL npc_kroshiusAI : public ScriptedAI 
-{ 
-    uint32 m_uiKnockBackTimer; 
-	bool m_bIAmDeadKroshius; 
-	 
-    npc_kroshiusAI(Creature* pCreature) : ScriptedAI(pCreature) 
-    { 
-        //TemporarySummon* p_unit = dynamic_cast<TemporarySummon*>(pCreature); 
-        //if (p_unit)
-        if(m_creature->isTemporarySummon())
-            m_bIAmDeadKroshius = false; 
-        else 
-            m_bIAmDeadKroshius = true; 
-
-        if (!m_bIAmDeadKroshius) 
-        { 
-            std::list<Creature*> lAddsList; 
-            GetCreatureListWithEntryInGrid(lAddsList, m_creature, NPC_KROSHIUS, 100.0f); 
-
-            // despawn dead krosius if present 
-            for(std::list<Creature*>::iterator itr = lAddsList.begin(); itr != lAddsList.end(); ++itr) 
-            { 
-	            //TemporarySummon* temp = dynamic_cast<TemporarySummon*>((*itr)); 
-                //if (!temp) 
-                if(!(*itr)->isTemporarySummon())
-                { 
-                    (*itr)->ForcedDespawn(0); 
-                    break; 
-	            } 
-	        } 
-	    } 
- 
-        Reset(); 
-	 
-	        if (!m_bIAmDeadKroshius) 
-	            DoScriptText(SAY_KROSHIUS_REVIVE, m_creature);      //Yell on "Revive" 
-	    } 
-	 
-	void Reset() 
-	{ 
-        if (!m_bIAmDeadKroshius) 
-        { 
-            m_uiKnockBackTimer=3000; 
-	        //Change faction to be attackable 
-            m_creature->setFaction(90); 
-            m_creature->RemoveFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_OOC_NOT_ATTACKABLE); 
-	        m_creature->RemoveFlag(UNIT_DYNAMIC_FLAGS, UNIT_DYNFLAG_TAPPED | UNIT_DYNFLAG_DEAD); 
-	    } 
-    } 
-	 
-    void JustRespawned() 
-    { 
-	        // workaround? core sets dynamicflags 0 on respawn 
-	        if (m_bIAmDeadKroshius) 
-	            m_creature->SetUInt32Value(UNIT_DYNAMIC_FLAGS, m_creature->GetCreatureInfo()->dynamicflags); 
-	}
-
-	void UpdateAI(const uint32 diff) 
-    { 
-	        //Return since we have no target 
-        if (!m_creature->SelectHostileTarget() || !m_creature->getVictim() || m_bIAmDeadKroshius) 
-            return; 
-	 
-	    if (m_uiKnockBackTimer<diff) 
-	    { 
-            DoCast(m_creature->getVictim(),SPELL_KNOCKBACK); 
-            m_uiKnockBackTimer = urand(10000, 16000); 
-	    }else m_uiKnockBackTimer -= diff; 
- 
-	    DoMeleeAttackIfReady(); 
-    } 
-}; 
- 
-CreatureAI* GetAI_npc_kroshius(Creature* pCreature) 
+enum
 {
-    return new npc_kroshiusAI(pCreature); 
-} 
+    NPC_KROSHIUS = 14467,
+    SPELL_KNOCKBACK = 10101,
+    SAY_KROSHIUS_REVIVE = -1000589,
+    EVENT_KROSHIUS_REVIVE = 8328,
+    FACTION_HOSTILE = 16,
+};
+
+struct MANGOS_DLL_DECL npc_kroshiusAI : public ScriptedAI
+{
+    npc_kroshiusAI(Creature* pCreature) : ScriptedAI(pCreature)
+    {
+        m_uiPhase = 0;
+        Reset();
+    }
+
+    uint64 m_uiPlayerGUID;
+    uint32 m_uiKnockBackTimer;
+    uint32 m_uiPhaseTimer;
+
+    uint8 m_uiPhase;
+
+    void Reset()
+    {
+        m_uiKnockBackTimer = urand(5000, 8000);
+        m_uiPlayerGUID = 0;
+
+        if (!m_uiPhase)
+        {
+            m_creature->setFaction(m_creature->GetCreatureInfo()->faction_A);
+            // TODO: Workaround? till better solution
+            m_creature->SetFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_OOC_NOT_ATTACKABLE);
+            m_creature->SetFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_PASSIVE);
+            m_creature->SetStandState(UNIT_STAND_STATE_DEAD);
+        }
+    }
+
+    void DoRevive(Player* pSource)
+    {
+        if (m_uiPhase)
+            return;
+
+        m_uiPhase = 1;
+        m_uiPhaseTimer = 2500;
+        m_uiPlayerGUID = pSource->GetGUID();
+
+        // TODO: A visual Flame Circle around the mob still missing
+    }
+
+    void JustDied(Unit* pKiller)
+    {
+        m_uiPhase = 0;
+    }
+
+    void UpdateAI(const uint32 uiDiff)
+    {
+        if (!m_uiPhase)
+            return;
+
+        if (m_uiPhase < 4)
+        {
+            if (m_uiPhaseTimer < uiDiff)
+            {
+                switch (m_uiPhase)
+                {
+                    case 1: // Revived
+                        m_creature->SetStandState(UNIT_STAND_STATE_STAND);
+                        m_uiPhaseTimer = 1000;
+                        break;
+                    case 2:
+                        DoScriptText(SAY_KROSHIUS_REVIVE, m_creature);
+                        m_uiPhaseTimer = 3500;
+                        break;
+                    case 3: // Attack
+                        m_creature->setFaction(FACTION_HOSTILE);
+                        // TODO workaround will better idea
+                        m_creature->RemoveFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_OOC_NOT_ATTACKABLE);
+                        m_creature->RemoveFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_PASSIVE);
+                        if (Player* pPlayer = m_creature->GetMap()->GetPlayer(m_uiPlayerGUID))
+                            if (m_creature->IsWithinDistInMap(pPlayer, 30.0f))
+                                AttackStart(pPlayer);
+                        break;
+                }
+                m_uiPhase++;
+            }
+            else
+                m_uiPhaseTimer -= uiDiff;
+        }
+        else
+        {
+            if (!m_creature->SelectHostileTarget() || !m_creature->getVictim())
+                return;
+
+            if (m_uiKnockBackTimer < uiDiff)
+            {
+                DoCastSpellIfCan(m_creature->getVictim(), SPELL_KNOCKBACK);
+                m_uiKnockBackTimer = urand(9000, 12000);
+            }
+            else
+                m_uiKnockBackTimer -= uiDiff;
+
+            DoMeleeAttackIfReady();
+        }
+    }
+};
+
+CreatureAI* GetAI_npc_kroshius(Creature* pCreature)
+{
+    return new npc_kroshiusAI(pCreature);
+}
+
+bool ProcessEventId_npc_kroshius(uint32 uiEventId, Object* pSource, Object* pTarget, bool bIsStart)
+{
+    if (uiEventId == EVENT_KROSHIUS_REVIVE)
+    {
+        if (pSource->GetTypeId() == TYPEID_PLAYER)
+            if (Creature* pKroshius = GetClosestCreatureWithEntry((Player*)pSource, NPC_KROSHIUS, 20.0f))
+                if (npc_kroshiusAI* pKroshiusAI = dynamic_cast<npc_kroshiusAI*>(pKroshius->AI()))
+                    pKroshiusAI->DoRevive((Player*)pSource);
+
+        return true;
+    }
+    return false;
+}
 
 void AddSC_felwood()
 {
@@ -446,8 +490,9 @@ void AddSC_felwood()
     newscript->pChooseReward = &ChooseReward_npc_niby_the_almighty;
     newscript->RegisterSelf();
 
-    newscript = new Script; 
- 	newscript->Name = "npc_kroshius"; 
- 	newscript->GetAI = &GetAI_npc_kroshius; 
- 	newscript->RegisterSelf(); 
+    newscript = new Script;
+    newscript->Name = "npc_kroshius";
+    newscript->GetAI = &GetAI_npc_kroshius;
+    newscript->pProcessEventId = &ProcessEventId_npc_kroshius;
+    newscript->RegisterSelf();
 }

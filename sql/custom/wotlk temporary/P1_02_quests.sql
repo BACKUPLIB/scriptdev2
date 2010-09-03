326957d3a6f5adc81ff0458956cb969b99015a04
pasdVn <pasdVn3@gmx.de>
2010-09-03 02:05:49 +0200

    [OV179] Added quest query for wotlk phase I.
    Also unified structure of queries and fixed emblem query.
    
    Thx to Venat and eumel!

---
+++ sql/custom/wotlk temporary/P1_02_quests.sql
@@ -0,0 +1,39 @@
+/*  Quests Phase I  */
+
+-- quest start
+DELETE FROM `creature_questrelation` WHERE `id` = 20735;
+INSERT INTO `creature_questrelation` (`id`,`quest`) VALUES
+(20735, 13245),
+(20735, 13246),
+(20735, 13247),
+(20735, 13248),
+(20735, 13249),
+(20735, 13250),
+(20735, 13251),
+(20735, 13252),
+(20735, 13253),
+(20735, 13254),
+(20735, 13255),
+(20735, 13256);
+
+--quest end
+DELETE FROM `creature_involvedrelation` WHERE `entry` =20735;
+INSERT INTO `creature_involvedrelation`(`id`,`quest`) VALUES
+(20735, 13245),
+(20735, 13246),
+(20735, 13247),
+(20735, 13248),
+(20735, 13249),
+(20735, 13250),
+(20735, 13251),
+(20735, 13252),
+(20735, 13253),
+(20735, 13254),
+(20735, 13255),
+(20735, 13256);
+
+
+/*
+    NPC 20735 holds the daily dungeon quests for all phases. Some Quests are
+    added and loot changes (see emblem query).
+*/

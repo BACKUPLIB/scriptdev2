550062d8816819d87110d598a6bdbd5406b72e05
Brueggus <brueggus@orangevir.us>
2010-09-11 20:17:54 +0200

    fixed crappy .sql by kelthuzad

--- sql/custom/wotlk temporary/mangos_close_instances.sql
+++ sql/custom/wotlk temporary/mangos_close_instances.sql
@@ -21,6 +21,6 @@ UPDATE `areatrigger_teleport` SET `required_level` = 90 WHERE `target_map` IN (6
 
 -- close/despawn instances of later phases
 -- despawn naxxramas 25, obsidiansanctum 25
-UPDATE `creature` SET `spawnMask` = `spawnMask` & 1 WHERE `map` IN (533,615);
+UPDATE `creature` SET `spawnMask` = `spawnMask` &~ 2 WHERE `map` IN (533,615);
 -- close instances eye of eternity, ulduar, onyxias lair, trial of the crusader, icecrown citadel, culling of stratholme
 UPDATE `areatrigger_teleport` SET `required_level` = 90 WHERE `target_map` IN (616,603,249,649,631,595);

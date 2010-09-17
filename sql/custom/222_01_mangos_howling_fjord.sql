5010ea2b396bf6c0f9c38f7fb74cd0be51a62fd9
[DEV]kelthuzad <kel_thalas@web.de>
2010-09-17 22:30:23 +0200

    [OV222] fix quest 11255

---
+++ sql/custom/222_01_mangos_howling_fjord.sql
@@ -0,0 +1,17 @@
+-- howling fjord fixes
+
+-- fix quest 11255
+UPDATE `gameobject_template` SET `type` = 1 WHERE `entry` IN (186566,186567);
+
+DELETE FROM `gameobject_scripts` WHERE `id` IN(60407,60414,60426,60432,60438,60452,60459,60471,60485,60511);
+INSERT INTO `gameobject_scripts` (`id`,`delay`,`command`,`datalong`,`datalong2`) VALUES 
+(60407,0,8,24124,1),
+(60414,0,8,24124,1),
+(60426,0,8,24124,1),
+(60432,0,8,24124,1),
+(60438,0,8,24124,1),
+(60452,0,8,24124,1),
+(60459,0,8,24124,1),
+(60471,0,8,24124,1),
+(60485,0,8,24124,1),
+(60511,0,8,24124,1);

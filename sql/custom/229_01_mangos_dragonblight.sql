d6aa6810e44feb0eda843de85dc97a70bf1d70a4
Carsten <C.Bosner@gmx.de>
2010-09-23 17:44:56 +0200

    [OV251] add npc 26589
    * mantis bug 2966
    * thx eumel!

--- sql/custom/229_01_mangos_dragonblight.sql
+++
@@ -1,8 +0,0 @@
--- fixes for dragonblight
-
--- fix quest 12173
-UPDATE `creature_template` SET `unit_flags` = 512 WHERE `entry` = 27135;
-UPDATE `quest_template` SET `Method` = 2 WHERE `entry` = 12173;
-
--- fix quest 12111
-UPDATE `quest_template` SET `ReqItemId1` = 36818, `ReqItemCount1` = 1, ReqSourceId1 = 0, ReqSpellCast1 = 47628, ReqSpellCast2 = 47628, ReqCreatureOrGOId1 = 26615, ReqCreatureOrGOId2 = 26482 WHERE `entry` = 12111;

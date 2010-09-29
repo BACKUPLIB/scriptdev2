1f24932e6302bc1170e1a8674f125593b00457ea
[DEV]kelthuzad <kel_thalas@web.de>
2010-09-29 16:54:19 +0200

    [OV267] better fix for quest 12978

--- sql/custom/267_01_mangos_storm_peaks.sql
+++ sql/custom/267_01_mangos_storm_peaks.sql
@@ -9,9 +9,7 @@ INSERT INTO `event_scripts` (`id`,`delay`,`command`,`datalong`,`datalong2`) VALU
 -- fix quest 12978
 UPDATE `quest_template` SET `ReqCreatureOrGOId1` = 29380 WHERE `entry` = 12978;
 DELETE FROM `creature_ai_scripts` WHERE `id` IN (2937404,2937002);
-INSERT INTO `creature_ai_scripts` (`id`,`creature_id`,`event_type`,`event_inverse_phase_mask`,`event_chance`,`event_flags`,`action1_type`,`action1_param1`,`action1_param2`,`comment`) VALUES 
-(2937404,29374,6,0,100,1,33,29380,1,"Stormforged Magus - Give Credit for Quest 12978"),
-(2937002,29370,6,0,100,1,33,29380,1,"Stormforged Champion - Give Credit for Quest 12978");
+UPDATE `creature_template` SET `KillCredit1` = 29380 WHERE `entry` IN (29370,29374);
 
 -- fix quest 12957
 UPDATE `creature_template` SET `ScriptName` = "npc_captive_mechagnome" WHERE `entry` = 29384;

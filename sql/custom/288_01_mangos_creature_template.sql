5888d12f40cefaf5d76c7ab197493c35801a38c0
Carsten <C.Bosner@gmx.de>
2010-10-06 16:30:16 +0200

    [OV288] fix npc 54343
    * thanks to Alphawolf
    * mantis bug #0003033

--- sql/custom/288_01_mangos_creature_template.sql
+++ sql/custom/288_01_mangos_creature_template.sql
@@ -3,3 +3,10 @@ UPDATE `creature_template` SET `minhealth` = '64', `maxhealth` = '64' WHERE `ent
 
 -- fix NPC 40295
 UPDATE `creature_template` SET `minhealth` = '64', `maxhealth` = '64' WHERE `entry` =40295; 
+
+-- fix NPC 54343
+-- not perfect, but it works!
+UPDATE `creature_template` SET `modelid_1` = '21733', `minhealth` = '4270',
+`maxhealth` = '4270',
+`minmana` = '3990',
+`maxmana` = '3990' WHERE `entry` =40281;

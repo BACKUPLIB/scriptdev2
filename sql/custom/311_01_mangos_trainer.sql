c73c1af85ce4711fd3642aadfcafe7c585ae159b
Carsten <C.Bosner@gmx.de>
2010-10-16 14:11:09 +0200

    [OV311] fix spellskills for first aid
    * mantis bug #0003231

---
+++ sql/custom/311_01_mangos_trainer.sql
@@ -0,0 +1,6 @@
+-- trainer fixxes
+
+-- Frostweave Bandage & heavy netherweave bandage skillfix
+-- mantis bug #0003231
+UPDATE `npc_trainer` SET `reqskillvalue` = 330 WHERE `spell` = 27033;
+UPDATE `npc_trainer` SET ´reqskillvalue` = 350 WHERE `spell` = 45545;

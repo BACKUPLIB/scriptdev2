04266486a1b9bfa5818c3ca1fc1c12fb53b5f259
Carsten <C.Bosner@gmx.de>
2010-10-16 14:15:03 +0200

    fix syntax mistake

--- sql/custom/311_01_mangos_trainer.sql
+++ sql/custom/311_01_mangos_trainer.sql
@@ -3,4 +3,4 @@
 -- Frostweave Bandage & heavy netherweave bandage skillfix
 -- mantis bug #0003231
 UPDATE `npc_trainer` SET `reqskillvalue` = 330 WHERE `spell` = 27033;
-UPDATE `npc_trainer` SET ´reqskillvalue` = 350 WHERE `spell` = 45545;
+UPDATE `npc_trainer` SET `reqskillvalue` = 350 WHERE `spell` = 45545;

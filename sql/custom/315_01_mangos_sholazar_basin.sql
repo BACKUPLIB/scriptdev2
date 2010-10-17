4e9a18014306afd413c1ec269611949f4f74e2e6
[DEV]kelthuzad <kel_thalas@web.de>
2010-10-17 15:11:34 +0200

    [OV315] fix gossip-bug with npc 27801

--- sql/custom/315_01_mangos_sholazar_basin.sql
+++ sql/custom/315_01_mangos_sholazar_basin.sql
@@ -52,7 +52,7 @@ INSERT INTO `gossip_menu` (`entry`,`text_id`, `cond_1`, `cond_1_val_1` ) VALUES
 DELETE FROM `gossip_menu_option` WHERE `menu_id` IN (27800, 27801, 27802, 27803);
 INSERT INTO `gossip_menu_option` (`menu_id`, `id`, `option_icon`, `option_text`, `option_id`, `npc_option_npcflag`, `action_menu_id`, `action_script_id` ,`cond_1`, `cond_1_val_1`, `cond_1_val_2`) VALUES 
 ('27800', '0', '0', 'I want to stop the Scourge as much as you do. How can I help?', '1', '1', '27801' , '0', '9', '12621', '0'),
-('27800', '1', '0', 'GOSSIP_OPTION_QUESTGIVER', '2', '2', '0', '0', '19', '12621', '0'),
+('27800', '1', '0', 'GOSSIP_OPTION_QUESTGIVER', '2', '2', '0', '0', '0', '0', '0'),
 ('27801', '0', '0', 'You can trust me. I am no friend of the Lich King.', '1', '1', '27802' , '0', '9', '12621', '0'),
 ('27802', '0', '0', 'I will not fail.', '1', '1', '27803' , '126211', '9', '12621', '0'),
 ('27803', '0', '0', 'GOSSIP_OPTION_QUESTGIVER', '2', '2', '0', '0', '0', '0', '0');

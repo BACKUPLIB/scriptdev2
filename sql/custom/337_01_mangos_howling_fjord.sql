519feab43dde7e8f03f396d45fd9ec477be21aa6
Jeiy <pfefferminz1@gmx.de>
2010-10-27 21:31:39 +0200

    fix sql typo

--- sql/custom/337_01_mangos_howling_fjord.sql
+++ sql/custom/337_01_mangos_howling_fjord.sql
@@ -114,6 +114,6 @@ UPDATE `creature_template` SET `AIName` = 'EventAI' WHERE `entry` IN (24009, 240
 UPDATE `spell_script_target` SET `type` = '1' WHERE `entry` =42793;
 DELETE FROM `creature_ai_scripts` WHERE `id` IN (24009, 24010);
 INSERT INTO `creature_ai_scripts` (`id`, `creature_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_type`, `action1_param1`, `action1_param2`, `action1_param3`, `action2_type`, `action2_param1`, `action2_param2`, `action2_param3`, `action3_type`, `action3_param1`, `action3_param2`, `action3_param3`, `comment`) VALUES 
-('24009', '24009', '8', '0', '100', '0', '42793', '-1', '0', '0', '37', '0', '0', '0', '33', '24008', '6', '0', '0', '0', '0', '0', ''),
-('24010', '24010', '8', '0', '100', '0', '42793', '-1', '0', '0', '37', '0', '0', '0', '33', '24008', '6', '0', '0', '0', '0', '0', '');
+('2400901', '24009', '8', '0', '100', '0', '42793', '-1', '0', '0', '37', '0', '0', '0', '33', '24008', '6', '0', '0', '0', '0', '0', ''),
+('2401001', '24010', '8', '0', '100', '0', '42793', '-1', '0', '0', '37', '0', '0', '0', '33', '24008', '6', '0', '0', '0', '0', '0', '');
 

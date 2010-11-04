c88f7b704b17b284acb4001f2fdfd909f2e7e262
Jeiy <pfefferminz1@gmx.de>
2010-11-04 11:34:18 +0100

    remove reputation for one more NPC

--- sql/custom/323_03_mangos_azjol_nerub.sql
+++ sql/custom/323_03_mangos_azjol_nerub.sql
@@ -21,4 +21,4 @@ INSERT INTO creature_ai_scripts (id, creature_id, event_type, event_chance, even
 UPDATE creature_template SET mechanic_immune_mask = '617299803' WHERE entry IN(28684,28921,29120,31561,31610,31611);
 
 -- remote possible reputation bug
-DELETE FROM `creature_onkill_reputation` WHERE `creature_id` = 31647;
+DELETE FROM `creature_onkill_reputation` WHERE `creature_id` IN (31614,31647);

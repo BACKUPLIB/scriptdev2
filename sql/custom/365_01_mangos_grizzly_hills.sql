7dccbfec252b8aa418dfabaabf3aea582146225d
Jeiy <pfefferminz1@gmx.de>
2010-11-12 17:06:29 +0100

    [OV365] fix quests 12203 & 12185
    * thx eumel
    * mantis bug #0003006

--- sql/custom/365_01_mangos_grizzly_hills.sql
+++ sql/custom/365_01_mangos_grizzly_hills.sql
@@ -51,3 +51,31 @@ INSERT INTO `spell_scripts` (`id`,`delay`,`command`,`datalong`,`datalong2`,`comm
 -- despawn some NPCs
 UPDATE `creature` SET `spawnMask` = 0 WHERE guid IN (114879,114891,114892,114893,114894,114897,114898,114899,114902,114912,114913,114914,114917,114918,114919,114920,114923,114924,114926,114928,114929,114930,
 114931,116183,116199,116563,116564,116566,116567,116568,116568,116569,116574,116579,116580);
+
+-- fix quests 12203 & 12185
+UPDATE `creature_template` SET `AIName` = 'EventAI' WHERE `entry` =27212;
+
+UPDATE `gameobject_template` SET `data2` = '60046' WHERE `entry` =188596;
+
+DELETE FROM `event_scripts` WHERE `id` =60046;
+INSERT INTO `event_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `comments`) VALUES 
+('60046', '0', '10', '27212', '120000', '0', '0', '0', '0', '0', '0', '0', '3404.324219', '-5394.511230', '270.199677', '1.148978', 'Image of Loken - Spawn'),
+('60046', '22', '8', '27212', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Loken Podest - Quest Credit');
+
+DELETE FROM `creature_ai_scripts` WHERE `creature_id` =27212;
+INSERT INTO `creature_ai_scripts` 
+(`id`, `creature_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_type`, `action1_param1`, `action1_param2`, `action1_param3`, `action2_type`, `action2_param1`, `action2_param2`, `action2_param3`, `action3_type`, `action3_param1`, `action3_param2`, `action3_param3`, `comment`) VALUES 
+('2721201', '27212', '1', '0', '100', '0', '1000', '1000', '0', '0', '1', '-2000', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Image of Loken - Text'),
+('2721202', '27212', '1', '0', '100', '0', '6000', '6000', '0', '0', '1', '-2001', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Image of Loken - Text'),
+('2721203', '27212', '1', '0', '100', '0', '11000', '11000', '0', '0', '1', '-2002', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Image of Loken - Text'),
+('2721204', '27212', '1', '0', '100', '0', '16000', '16000', '0', '0', '1', '-2003', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Image of Loken - Text'),
+('2721205', '27212', '1', '0', '100', '0', '21000', '21000', '0', '0', '1', '-2004', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Image of Loken - Text');
+
+DELETE FROM `creature_ai_texts` WHERE `entry` IN('-2000', '-2001', '-2002', '-2003', '-2004');
+INSERT INTO `creature_ai_texts` (`entry`, `content_default`, `content_loc1`, `content_loc2`, `content_loc3`, `content_loc4`, `content_loc5`, `content_loc6`, `content_loc7`, `content_loc8`, `sound`, `type`, `language`, `emote`, `comment`) VALUES 
+('-2000', 'You''re late, overseer.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '0', '0', '0', '0', NULL),
+('-2001', 'Destiny will not wait. Your craftsmen must increase their production of the war golems before the stone giant leader and his army reach Ulduar.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '0', '0', '0', '0', NULL),
+('-2002', 'Tell your rune-smiths to continue converting what stone giants you can. Those that will not submit must be destroyed.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '0', '0', '0', '0', NULL),
+('-2003', 'If the stone giants interfere with our work at Ulduar, I will hold you and your thane responsible. Mortal must not be allowed to come to the aid of the giants.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '0', '0', '0', '0', NULL),
+('-2004', 'Return to your duties, overseer. Be certain to impress upon your workers the urgency of their tasks.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '0', '0', '0', '0', NULL);
+

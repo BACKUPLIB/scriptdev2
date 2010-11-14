c23f348a5916143a05d64a4ba569260c3f80a680
[DEV]kelthuzad <kel_thalas@web.de>
2010-11-14 12:36:01 +0100

    [OV370] refixt [OV304]
    * mantis Bug #0003132

--- sql/custom/370_01_mangos_hellfire_peninsula.sql
+++ sql/custom/370_01_mangos_hellfire_peninsula.sql
@@ -1,7 +1,8 @@
 -- fixes for hellfire peninsula
 
 -- fix npc 16852
-UPDATE creature SET DeathState = 1 WHERE id = 16852;
+UPDATE creature SET DeathState = 0 WHERE id = 16852; -- apply this line once to revert wrong fix
+UPDATE `creature_template` SET `dynamicflags` = `dynamicflags` | 32 WHERE entry = 16852;
 
 -- fix mantis bug 2570
 UPDATE `quest_template` SET `ReqSpellCast1` = 0 WHERE `entry` = 11515;

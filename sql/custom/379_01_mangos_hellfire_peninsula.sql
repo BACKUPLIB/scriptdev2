4078d5d653b45a2983d0e4c4f8472552b209ed36
[DEV]kelthuzad <kel_thalas@web.de>
2010-11-20 20:34:16 +0100

    [OV379] fix quest 9545

--- sql/custom/379_01_mangos_hellfire_peninsula.sql
+++ sql/custom/379_01_mangos_hellfire_peninsula.sql
@@ -1,8 +1,7 @@
 -- fixes for hellfire peninsula
 
 -- fix npc 16852
-UPDATE creature SET DeathState = 0 WHERE id = 16852; -- apply this line once to revert wrong fix
-UPDATE `creature_template` SET `dynamicflags` = `dynamicflags` | 32 WHERE entry = 16852;
+UPDATE `creature_template` SET `dynamicflags` = `dynamicflags` | 32, `type_flags` = `type_flags` | 1024 WHERE entry = 16852;
 
 -- fix mantis bug 2570
 UPDATE `quest_template` SET `ReqSpellCast1` = 0 WHERE `entry` = 11515;
@@ -10,3 +9,6 @@ UPDATE `quest_template` SET `ReqSpellCast1` = 0 WHERE `entry` = 11515;
 -- fix quest 10909
 UPDATE `gameobject_template` SET flags = 5 WHERE `entry` = 185298;
 UPDATE `creature_template` SET `ScriptName` = "npc_anchronite_relic_bunny" WHERE `entry` = 22444;
+
+-- fix quest 9545
+UPDATE `quest_template` SET `SpecialFlags` = 0 WHERE `entry` = 9545;

c6ee02f68f8c9a3018a80965883117ebb51969f1
[DEV]kelthuzad <kel_thalas@web.de>
2010-11-24 09:13:41 +0100

    [OV381] fix quest 11379
    * mantis Bug #0003387

---
+++ sql/custom/381_01_mangos_shattrat.sql
@@ -0,0 +1,5 @@
+UPDATE quest_template SET ExclusiveGroup=11362 WHERE entry IN (11384, 11382, 11368, 11378, 11374, 11499, 11370, 11372, 11369, 11388, 11362, 11363, 11375, 11354, 11386, 11373);
+
+-- fix quest 11379
+DELETE FROM spell_scripts WHERE id = 43723;
+INSERT INTO spell_scripts (`id`,`delay`,`command`,`datalong`,`datalong2`,`comments`) VALUES (43723,0,15,43753,1,"Quest 11379 - create quest item");

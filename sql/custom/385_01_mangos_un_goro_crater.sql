de23739c8f386c0ab2d9f4027fe394d7611b079f
Jeiy <pfefferminz1@gmx.de>
2010-11-28 13:36:28 +0100

    [OV385] fix quests 9051, 12547 & 13007
    * mantis bug #0003429
    * mantis bug #0003414
    * mantis bug #0003357
    * thx eumel (:

---
+++ sql/custom/385_01_mangos_un_goro_crater.sql
@@ -0,0 +1,12 @@
+﻿-- un goro crater fixes
+
+-- fix quest 9051
+UPDATE `quest_template` SET `ReqCreatureOrGOId1` = '6498' WHERE `entry` =9051;
+DELETE FROM `creature_ai_scripts` WHERE `id` =649802;
+INSERT INTO `creature_ai_scripts` (`id`, `creature_id`, `event_type`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `action1_type`, `action1_param1`, `action1_param2`, `comment`) VALUES
+('649802', '6498', '8', '100', '1', '7211', '-1', '33', '6498', '6', 'Quest Credit for Quest 9051'); 
+
+-- fix NPC 28601 & quest 12547
+DELETE FROM `creature` WHERE `guid` =200041;
+INSERT INTO `creature` (`guid`, `id`, `map`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`) VALUES 
+('200041', '28601', '1', '-6027.365723', '-1250.199097', '-146.764404', '3.122257', '300');
--- sql/custom/385_02_mangos_storm_peaks.sql
+++ sql/custom/385_02_mangos_storm_peaks.sql
@@ -1,4 +1,4 @@
--- fixes for storm peak
+﻿-- fixes for storm peak
 
 -- fix quest 12843 (they took our men)
 UPDATE `gameobject_template` SET `data2` = 191544 WHERE `entry` = 191544;
@@ -176,3 +176,9 @@ UPDATE `creature_template` SET `AIName` ='EventAI' WHERE `entry` =30035;
 DELETE FROM `creature_ai_scripts` WHERE `creature_id` =30035;
 INSERT INTO `creature_ai_scripts` (`id`, `creature_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_type`, `action1_param1`, `action1_param2`, `action1_param3`, `action2_type`, `action2_param1`, `action2_param2`, `action2_param3`, `action3_type`, `action3_param1`, `action3_param2`, `action3_param3`, `comment`) VALUES
 ('3003501', '30035', '8', '0', '100', '1', '55797', '-1', '20000', '30000', '33', '30035', '6', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Quest Credit for Quest 12937');
+
+-- fix NPC 30300 & quest 13007
+DELETE FROM `creature` WHERE `guid` =200042;
+INSERT INTO `creature` (`guid`, `id`, `map`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`) VALUES 
+('200042', '30300', '571', '8206.556641', '-97.213173', '846.836060', '0.095819', '300');
+

85a8b1d884dc03e01f4f300b32374dad45b6067b
unknown <Donner-Alphawolf@.(none)>
2010-12-15 23:35:19 +0100

    [OV396] Fix Quest 12548
    * mantis bug #0003270
    * mantis bug #0003348

--- sql/custom/396_01_mangos_sholazar_basin.sql
+++ sql/custom/396_01_mangos_sholazar_basin.sql
@@ -89,3 +89,11 @@ INSERT INTO `gossip_scripts` (`id`,`delay`,`command`,`datalong`,`datalong2`,`com
 ('52542', '0', '15', '52542', '0', 'add Item 38619 Goregek\'s Shackles'),
 ('52544', '0', '15', '52544', '0', 'add Item 38621 Dajik\'s Worn Chalk'),
 ('52545', '0', '15', '52545', '0', 'add Item 38512 Zepik\'s Hunting Horn');
+
+-- fix quest 12548
+DELETE FROM areatrigger_teleport WHERE id ='5046';
+INSERT INTO areatrigger_teleport (id, NAME, required_level, required_item, required_item2, heroic_key, heroic_key2, required_quest_done, required_quest_done_heroic, required_failed_text, target_map, target_position_x, target_position_y, target_position_z, target_orientation)VALUES
+('5046', 'The_Waygate_Sholazar', '1', '0', '0', '0', '0', '12559', '0', '0', '1', '-6161.8', '-1332.13', '-168.965', '2.35776');
+DELETE FROM areatrigger_teleport WHERE id ='5047';
+INSERT INTO areatrigger_teleport (id, NAME, required_level, required_item, required_item2, heroic_key, heroic_key2, required_quest_done, required_quest_done_heroic, required_failed_text, target_map, target_position_x, target_position_y, target_position_z, target_orientation)VALUES
+('5047', 'The_Waygate_Ungoro', '1', '0', '0', '0', '0', '12559', '0', '0', '571', '4885.993', '5175.694', '-84.863', '0.450570');

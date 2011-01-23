5ed2fe3b377442b3739f5ff2b112d3d7a12ece0d
Alphawolf <Schroeter_1979@web.de>
2011-01-23 23:46:17 +0100

    [OV427] update and reimplement script-text for nagrand

--- sql/custom/427_01_mangos_nagrand.sql
+++ sql/custom/427_01_mangos_nagrand.sql
@@ -1,6 +1,6 @@
 -- map nagrand fixes
 
---fix npc 19055 (Windroc Matriarch - remove unitflag 320)
+-- fix npc 19055 (Windroc Matriarch - remove unitflag 320)
 UPDATE creature_template SET unit_flags=0 WHERE entry=19055;
 
 -- fix npc 18209 (Kurenai Captive)

77900cf386eed8bcf2fb90e3f995084c9cd3fec6
Alphawolf <Schroeter_1979@web.de>
2011-01-23 21:15:31 +0100

    [OV434] update and reimplement ironforge
    only added for completeness

---
+++ sql/custom/423_01_mangos_ironforge.sql
@@ -0,0 +1,2 @@
+-- Quest 14111 fix: Nich blizzlike, aber eigentlich sogar schöner so!
+UPDATE `quest_template` SET `RewSpell` = '0', `RewSpellCast` = '0', `RewItemId1` = '46978', `RewItemCount1` = '1' WHERE `entry` = 14111;

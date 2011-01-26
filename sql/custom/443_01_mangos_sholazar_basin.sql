7473bd8bde82946af91f4eff3dadc0d47b85e6bc
Alphawolf <Schroeter_1979@web.de>
2011-01-26 20:17:24 +0100

    [OV443] update and reimplement sholazar basin

---
+++ sql/custom/443_01_mangos_sholazar_basin.sql
@@ -0,0 +1,72 @@
+-- map sholazar basin fixes
+
+-- fix quest 12688
+UPDATE `creature_template` SET `ScriptName` = 'npc_engineer_helice' WHERE entry = 28787;
+
+-- fix quest 12532
+DELETE FROM `creature` WHERE `id` = 28161;
+INSERT INTO `creature` (`id`, `map`, `spawnMask`, `phaseMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `DeathState`, `MovementType`) VALUES
+(28161, 571, 1, 1, 0, 0, 5190.4, 4571.54, -102.378, 3.80362, 25, 5, 0, 1, 0, 0, 1),
+(28161, 571, 1, 1, 0, 0, 5174.2, 4558.92, -102.739, 3.80362, 25, 5, 0, 1, 0, 0, 1),
+(28161, 571, 1, 1, 0, 0, 5179.45, 4557.77, -100.218, 0.808116, 25, 5, 0, 1, 0, 0, 1),
+(28161, 571, 1, 1, 0, 0, 5174.86, 4546.15, -98.9565, 4.32278, 25, 5, 0, 1, 0, 0, 1),
+(28161, 571, 1, 1, 0, 0, 5168.98, 4534.39, -97.3293, 4.00077, 25, 5, 0, 1, 0, 0, 1),
+(28161, 571, 1, 1, 0, 0, 5156.71, 4540.77, -97.5804, 1.07594, 25, 5, 0, 1, 0, 0, 1),
+(28161, 571, 1, 1, 0, 0, 5212.14, 4553.01, -95.3657, 0.939282, 25, 5, 0, 1, 0, 0, 1),
+(28161, 571, 1, 1, 0, 0, 5246.05, 4561.46, -91.9413, 0.413066, 25, 5, 0, 1, 0, 0, 1),
+(28161, 571, 1, 1, 0, 0, 5239.75, 4562.96, -91.0187, 3.21223, 25, 5, 0, 1, 0, 0, 1),
+(28161, 571, 1, 1, 0, 0, 5263.64, 4564.33, -89.5545, 0.565433, 25, 5, 0, 1, 0, 0, 1),
+(28161, 571, 1, 1, 0, 0, 5274.27, 4559.52, -86.6077, 5.81975, 25, 5, 0, 1, 0, 0, 1),
+(28161, 571, 1, 1, 0, 0, 5317.8, 4536.26, -97.4372, 1.60373, 25, 5, 0, 1, 0, 0, 1),
+(28161, 571, 1, 1, 0, 0, 5317.58, 4543.02, -96.4277, 1.60373, 25, 5, 0, 1, 0, 0, 1),
+(28161, 571, 1, 1, 0, 0, 5313.79, 4522.64, -97.9109, 4.19476, 25, 5, 0, 1, 0, 0, 1),
+(28161, 571, 1, 1, 0, 0, 5324.13, 4508.97, -97.858, 1.26758, 25, 5, 0, 1, 0, 0, 1),
+(28161, 571, 1, 1, 0, 0, 5282.61, 4409.38, -96.2805, 4.88277, 25, 5, 0, 1, 0, 0, 1),
+(28161, 571, 1, 1, 0, 0, 5273.41, 4390.85, -96.2771, 3.83426, 25, 5, 0, 1, 0, 0, 1),
+(28161, 571, 1, 1, 0, 0, 5260.79, 4388.52, -95.2331, 3.29627, 25, 5, 0, 1, 0, 0, 1),
+(28161, 571, 1, 1, 0, 0, 5246.28, 4399.47, -95.2409, 2.49516, 25, 5, 0, 1, 0, 0, 1),
+(28161, 571, 1, 1, 0, 0, 5264.74, 4401.47, -94.8276, 0.107547, 25, 5, 0, 1, 0, 0, 1),
+(28161, 571, 1, 1, 0, 0, 5239.8, 4432.74, -92.6718, 1.87941, 25, 5, 0, 1, 0, 0, 1),
+(28161, 571, 1, 1, 0, 0, 5193.29, 4451.03, -90.1913, 2.52343, 25, 5, 0, 1, 0, 0, 1),
+(28161, 571, 1, 1, 0, 0, 5206.27, 4452.23, -90.7965, 0.0965526, 25, 5, 0, 1, 0, 0, 1);
+
+-- fix quest 12544
+DELETE FROM `event_scripts` WHERE `id` = 18481 AND `command` = 10 AND `datalong` = 2825;
+INSERT INTO `event_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `data_flags`, `dataint`, `x`, `y`, `z`, `o`) VALUES
+('18481','0','10','28256','120000','0','0','0','0','5218.2485','5776.6992','-71.2309','2.462964');
+UPDATE `creature_template` SET `ScriptName` = 'mob_voiceofnozronn' WHERE `entry` = 28256;
+
+-- fix quest 12531
+-- not the best way, but EventAI of this mob is corrupted somehow, so disable this part
+-- TODO: fix EventAI instead of removing this
+UPDATE `creature_ai_scripts` SET `creature_id` = 0 WHERE `id` IN (2808301,2808306,2808309);
+UPDATE creature_template SET unit_flags=0 WHERE entry=28083;
+
+-- set item 44117 not lootable
+DELETE FROM `reference_loot_template` WHERE `item` = 44117; 
+
+-- items for quests 12533, 12536
+UPDATE `quest_template` SET `SrcItemId` = '38621', `SrcItemCount` = '1' WHERE `entry` = '12533';
+UPDATE `quest_template` SET `SrcItemId` = '38512', `SrcItemCount` = '1' WHERE `entry` = '12536';
+UPDATE `creature_template` SET `gossip_menu_id` = '28138' WHERE `entry` = '28138';
+DELETE FROM `npc_text` WHERE `ID` = '28138';
+INSERT INTO `npc_text` (`ID`, `text0_0`) VALUES 
+('28138', 'Stay a while and listen.');
+DELETE FROM `locales_npc_text` WHERE `entry` = '28138';
+INSERT INTO `locales_npc_text` (`entry`, `Text0_0_loc3`) VALUES 
+('28138', 'Bleib eine Weile und höre zu.');
+DELETE FROM `gossip_menu` WHERE `entry` = '28138';
+INSERT INTO `gossip_menu` (`entry`,`text_id`, `cond_1`, `cond_1_val_1` ) VALUES 
+('28138', '28138', '0', '0');
+DELETE FROM `gossip_menu_option` WHERE `menu_id` = '28138';
+INSERT INTO `gossip_menu_option` (`menu_id`, `id`, `option_icon`, `option_text`, `option_id`, `npc_option_npcflag`, `action_menu_id`, `action_script_id` ,`cond_1`, `cond_1_val_1`, `cond_1_val_2`, `cond_2`, `cond_2_val_1`, `cond_2_val_2`, `cond_3`, `cond_3_val_1`, `cond_3_val_2`) VALUES 
+('28138', '0', '0', 'GOSSIP_OPTION_QUESTGIVER', '2', '2', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0'),
+-- todo update gossiptext (not blizzlike)
+('28138', '1', '0', 'I have lost Goregek\'s Shackles', '1', '1', '28138', '52542', '8', '12528', '0', '16', '38619', '1', '22', '12530', '0'),
+('28138', '2', '0', 'I have lost Dajik\'s Worn Chalk', '1', '1', '28138', '52544', '8', '12530', '0', '16', '38621', '1', '22', '12536', '0'),
+('28138', '3', '0', 'I have lost Zepik\'s Hunting Horn', '1', '1', '28138', '52545', '8', '12531', '0', '16', '38512', '1', '22', '12539', '0');
+DELETE FROM `gossip_scripts` WHERE `id` IN ( '52542', '52544','52545') ;
+INSERT INTO `gossip_scripts` (`id`,`delay`,`command`,`datalong`,`datalong2`,`comments`)  VALUES 
+('52542', '0', '15', '52542', '0', 'add Item 38619 Goregek\'s Shackles'),
+('52544', '0', '15', '52544', '0', 'add Item 38621 Dajik\'s Worn Chalk'),
+('52545', '0', '15', '52545', '0', 'add Item 38512 Zepik\'s Hunting Horn');
---
+++ sql/custom/443_02_scriptdev2_sholazar_basin.sql
@@ -0,0 +1,28 @@
+-- map sholazar basin fixes
+
+-- fix quest 12688
+DELETE FROM `script_waypoint` WHERE `entry` = 28787;
+INSERT INTO `script_waypoint` (entry, pointid, location_x, location_y, location_z, waittime, point_comment) VALUES
+(28787, 1, 5926.399902, 5373.770020, -98.901802, 6.047510, NULL),
+(28787, 2, 5928.290039, 5377.782715, -99.031799, 1.500054, NULL),
+(28787, 3, 5916.380371, 5383.412109, -106.309975, 2.691506, NULL),
+(28787, 4, 5904.631348, 5391.954590, -104.998711, 2.467674, NULL),
+(28787, 5, 5902.749512, 5404.356445, -96.881393, 1.427021, NULL),
+(28787, 6, 5879.392090, 5393.845215, -92.679749, 3.555451, NULL),
+(28787, 7, 5876.169922, 5368.767578, -96.532730, 4.483795, NULL),
+(28787, 8, 5864.340332, 5357.849121, -98.547554, 3.882966, NULL),
+(28787, 9, 5834.825684, 5327.166504, -99.319473, 3.953652, NULL),
+(28787, 10, 5816.554199, 5305.958008, -97.237198, 4.004703, "quest complete");
+DELETE FROM `script_texts` WHERE `entry` IN (-1039981,-1039982);
+INSERT INTO `script_texts` (`entry`,`content_default`,`type`,`language`,`comment`) VALUES
+(-1039981,"Let's get the hell out of here.",0,0,"engineer helice SAY START"),
+(-1039982,"We made it! Thank you for getting me out of that hell hole. Tell Hemet to expect me!",0,0,"engineer helice SAY END");
+
+-- fix quest 12544
+DELETE FROM `script_texts` WHERE `entry` IN (-1594190,-1594191,-1594192,-1594193,-1594194);
+INSERT INTO `script_texts` (`entry`, `content_default`, `content_loc3`, `sound`, `type`, `language`, `emote`, `comment`) VALUES
+('-1594190','Also hat das Orakel beschlossen, Euch zu helfen, Fremder. Nun gut, ich werde mein Wissen mit Euch teilen.','Also hat das Orakel beschlossen, Euch zu helfen, Fremder. Nun gut, ich werde mein Wissen mit Euch teilen.','0','0','0','0','SAY_NOZRONN1'),
+('-1594191','Euer Gefährte sucht den Splitterhornpatriarchen Farunn, der sich von seinen Brüdern fernhält.','Euer Gefährte sucht den Splitterhornpatriarchen Farunn, der sich von seinen Brüdern fernhält.','0','0','0','0','SAY_NOZRONN2'),
+('-1594192','Farunn durchstreift die Länder nördlich dessen, was Euer Volk die Bittertidenfälle nennt. Ihr werdet ihn allein vorfinden, aber Ihr werdet sehen, dass er keiner Verteidiger bedarf.','Farunn durchstreift die Länder nördlich dessen, was Euer Volk die Bittertidenfälle nennt. Ihr werdet ihn allein vorfinden, aber Ihr werdet sehen, dass er keiner Verteidiger bedarf.','0','0','0','0','SAY_NOZRONN3'),
+('-1594193','Kehrt zu Eurem Gefährten zurück. Sagt ihm, dass er losgehen und in Farunn seinen Preis einfordern soll.','Kehrt zu Eurem Gefährten zurück. Sagt ihm, dass er losgehen und in Farunn seinen Preis einfordern soll.','0','0','0','0','SAY_NOZRONN4'),
+('-1594194','Sobald er dies getan hat, sagt ihm, dass er dieses Land friedlich verlassen soll. Er gehört hier nicht her.','Sobald er dies getan hat, sagt ihm, dass er dieses Land friedlich verlassen soll. Er gehört hier nicht her.','0','0','0','0','SAY_NOZRONN5');

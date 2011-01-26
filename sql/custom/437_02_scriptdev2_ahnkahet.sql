c51e765fda30f203f5b1bb03c03b0b6d05fa2b68
[DEV]kelthuzad <kel_thalas@web.de>
2011-01-26 10:30:38 +0100

    sql typo

--- sql/custom/437_02_scriptdev2_ahnkahet.sql
+++ sql/custom/437_02_scriptdev2_ahnkahet.sql
@@ -8,5 +8,5 @@ INSERT INTO `script_texts` (`entry`,`content_default`,`sound`,`type`,`language`,
 (-1619035, 'Ywaq puul skshgn: on'ma yeh'glu zuq.', 14045, 1, 0, 0, 'volazj SAY_SLAY_1'),
 (-1619036, 'Ywaq ma phgwa\'cul hnakf.', 14046, 1, 0, 0, 'volazj SAY_SLAY_2'),
 (-1619037, 'Ywaq maq oou; ywaq maq ssaggh. Ywaq ma shg\'fhn.', 14047, 1, 0, 0, 'volazj SAY_SLAY_3'),
-(-1619038, 'Iilth vwah, uhn'agth fhssh za.', 14048, 1, 0, 0, 'volazj SAY_DEATH_1'),
+(-1619038, 'Iilth vwah, uhn\'agth fhssh za.', 14048, 1, 0, 0, 'volazj SAY_DEATH_1'),
 (-1619039, 'Where one falls, many shall take its place.', 14049, 1, 0, 0, 'volazj SAY_DEATH_2');

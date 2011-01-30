f04170331eb9cab4d4e785c9bb82787522365d0d
Alphawolf <Schroeter_1979@web.de>
2011-01-30 19:54:26 +0100

    [OV465] update and reimplement oculus

---
+++ sql/custom/465_01_mangos_oculus.sql
@@ -0,0 +1,4 @@
+-- instance oculus fixxes
+
+-- mechanic immune masks
+UPDATE creature_template SET mechanic_immune_mask = '617299803' WHERE entry IN(27654,27447,27655,27656,31558,31559,31560,31561);

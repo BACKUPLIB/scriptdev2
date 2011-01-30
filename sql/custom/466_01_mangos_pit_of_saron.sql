8013e8400acd95c47838b29838f089cb77117f79
Alphawolf <Schroeter_1979@web.de>
2011-01-30 20:00:05 +0100

    [OV466] update and reimplement pit of saron

---
+++ sql/custom/466_01_mangos_pit_of_saron.sql
@@ -0,0 +1,4 @@
+-- instance pit of saron fixxes
+
+-- mechanic immune masks
+UPDATE creature_template SET mechanic_immune_mask = '617299803' WHERE entry IN(36494,36476,36477,36658,36498,36938,37613,37627);

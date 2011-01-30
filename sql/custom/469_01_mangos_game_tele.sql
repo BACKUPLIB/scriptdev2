fe1742758058cde0d88367a92616f31cb98edc55
Alphawolf <Schroeter_1979@web.de>
2011-01-30 20:37:34 +0100

    [OV469] update and reimplement GM teleport locations

---
+++ sql/custom/469_01_mangos_game_tele.sql
@@ -0,0 +1,6 @@
+-- general teleport fixes
+
+-- add teleport point to storm peaks
+DELETE FROM game_tele WHERE id = 1422;
+INSERT INTO game_tele (id, position_x, position_y, position_z, orientation, map, name) VALUES 
+(1422, 7885.409668, -1481.846680, 1627.570923, 2.992628, 571, "Sturmgipfel");

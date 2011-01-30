87a7b9e059789216e9d4c2dc5f6d1a66e2d5ed54
Alphawolf <Schroeter_1979@web.de>
2011-01-30 17:40:16 +0100

    [OV459] update and reimplement ulduar

---
+++ sql/custom/459_01_mangos_ulduar.sql
@@ -0,0 +1,6 @@
+-- instance ulduar fixes
+
+-- kologarn
+DELETE FROM creature WHERE id IN (32933, 32934, 232933);
+INSERT INTO creature (guid, id, map, spawnMask, phaseMask, modelid, equipment_id, position_x, position_y, position_z, orientation, spawntimesecs, spawndist, currentwaypoint, curhealth, curmana, DeathState, MovementType) VALUES (9327991, 32933, 603, 3, 65535, 0, 0, 1799.68, -24.3599, 452.227, 3.14747, 604800, 0, 0, 543855, 0, 0, 0),
+(9327992, 32934, 603, 3, 65535, 0, 0, 1799.68, -24.3599, 452.227, 3.14747, 604800, 0, 0, 543855, 0, 0, 0);

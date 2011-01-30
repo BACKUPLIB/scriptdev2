a72ed302f94342fcf1806ade6395468d2ef82052
Alphawolf <Schroeter_1979@web.de>
2011-01-30 17:14:37 +0100

    [OV456] reimplement decreased need players for battelgrounds

---
+++ sql/custom/456_01_mangos_battlegrounds.sql
@@ -0,0 +1,7 @@
+-- general battleground fixxes
+
+-- decreased need players for battelgrounds
+UPDATE battleground_template SET MinPlayersPerTeam=12 WHERE id=1;
+UPDATE battleground_template SET MinPlayersPerTeam=5 WHERE id=2;
+UPDATE battleground_template SET MinPlayersPerTeam=7 WHERE id=3;
+UPDATE battleground_template SET MinPlayersPerTeam=7 WHERE id=7;

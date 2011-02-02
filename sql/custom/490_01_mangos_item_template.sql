c946f28ad1f510f18c6a8ec3ef85bc42a5e01051
Alphawolf <Schroeter_1979@web.de>
2011-02-02 19:39:29 +0100

    [OV490] update and reimplement item template fixes

---
+++ sql/custom/490_01_mangos_item_template.sql
@@ -0,0 +1,7 @@
+-- general item fixes
+
+-- mantis bug 2462
+     -- fix item 29109 remove spell 38164 because it proccs to often (should only procc in one mob fight)
+	UPDATE `item_template` SET `spellid_1` = 0, `spelltrigger_1` = 0 WHERE `entry` = 29109;
+     -- fix item 29108 remove spell 38164 because it proccs to often (should only procc in one mob fight)
+	UPDATE `item_template` SET `spellid_1` = 0, `spelltrigger_1` = 0 WHERE `entry` = 29108;

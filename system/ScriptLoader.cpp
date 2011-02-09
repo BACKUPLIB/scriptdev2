fde697e10af2dc482f09cc3e832e67ac0f3aa1e2
pasdVn <pasdVn3@gmx.de>
2011-02-09 22:23:38 +0100

    [OV497] Added support for scripting guardian pet AIs.
     * added AI for the dk's ghoul
     * also added some levelstatrs for the ghoul
    
     Reimplementation of [OV268]
    
      -- !!really dirty stuff!! just temporary --

--- system/ScriptLoader.cpp
+++ system/ScriptLoader.cpp
@@ -31,6 +31,7 @@ extern void AddSC_item_scripts();
 extern void AddSC_npc_professions();
 extern void AddSC_npcs_special();
 extern void AddSC_spell_scripts();
+extern void AddSC_pets();
 
 //eastern kingdoms
 extern void AddSC_blackrock_depths();                       //blackrock_depths
@@ -554,6 +555,7 @@ void AddScripts()
     AddSC_npc_professions();
     AddSC_npcs_special();
     AddSC_spell_scripts();
+    AddSC_pets();
 
     //eastern kingdoms
     AddSC_blackrock_depths();                               //blackrock_depths

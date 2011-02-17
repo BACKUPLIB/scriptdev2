986467c49260488ec95e05844bdd29f4989ce6a7
pasdVn <pasdVn3@gmx.de>
2011-02-17 21:36:50 +0100

    [OV503] One more small fix for Netherspite's Voidzones.

--- sql/custom/503_01_mangos_karazhan.sql
+++ sql/custom/503_01_mangos_karazhan.sql
@@ -3,8 +3,8 @@
 -- netherspite
 /* Portale: not attackable, not selectable, NullAI zur Sicherheit */
 UPDATE `creature_template` SET `unit_flags` = 0x02000002, `AIName` = 'NullAI' WHERE `entry` IN(17367,17368,17369);
-/* Voidzone: not attackable, not selectable, pacified, NullAI */
-UPDATE `creature_template` SET `unit_flags` = 0x02020002, `type_flags` = '1026', `AIName` = 'NullAI' WHERE `entry` =16697;
+/* Voidzone: not attackable, not selectable, pacified, no movement, NullAI */
+UPDATE `creature_template` SET `unit_flags` = 0x02020002, `type_flags` = '1026', `MovementType` = '0', `AIName` = 'NullAI' WHERE `entry` =16697;
 /*Voidzone: aoe dmg aura*/
 DELETE FROM `creature_template_addon` WHERE `entry` = 16697;
 INSERT INTO `creature_template_addon` (`entry`, `auras`) VALUES

8b1d76f9f9eef6e9f2ba808f667bd60356f2d063
[DEV]kelthuzad <kel_thalas@web.de>
2011-03-27 16:03:38 +0200

    fix arena vendors, decrease battleground minplayers

---
+++ sql/custom/orangevirus/wotlk_temporary/mangos_battleground_minplayers.sql
@@ -0,0 +1,5 @@
+-- decreased need players for battelgrounds
+UPDATE battleground_template SET MinPlayersPerTeam=12 WHERE id=1;
+UPDATE battleground_template SET MinPlayersPerTeam=5 WHERE id=2;
+UPDATE battleground_template SET MinPlayersPerTeam=7 WHERE id=3;
+UPDATE battleground_template SET MinPlayersPerTeam=7 WHERE id=7;

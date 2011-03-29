f1eadd8a7532a2209a7b2943cb029c60397507ae
[DEV]kelthuzad <kel_thalas@web.de>
2011-03-29 20:26:44 +0200

    decrease minplayers for battleground start

---
+++ sql/custom/orangevirus/003_01_mangos_battlegrounds.sql
@@ -0,0 +1,6 @@
+-- enable battlegrounds with less players
+
+UPDATE battleground_template SET MinPlayersPerTeam=12 WHERE id=1;
+UPDATE battleground_template SET MinPlayersPerTeam=5 WHERE id=2;
+UPDATE battleground_template SET MinPlayersPerTeam=7 WHERE id=3;
+UPDATE battleground_template SET MinPlayersPerTeam=7 WHERE id=7;

094bf20a346b012e3e8dfe72744d2da48a5be362
[DEV]kelthuzad <kel_thalas@web.de>
2011-04-24 20:59:09 +0200

    fix bc hero despawn query

---
+++ sql/custom/orangevirus/006_01_creature_despawn_bc_heros.sql
@@ -0,0 +1,2 @@
+-- close bc heros
+UPDATE `creature` SET `spawnMask` = 1 WHERE `map` IN (543, 547, 542, 546, 560, 555, 554, 558, 269, 553);
--- sql/custom/orangevirus/006_02_characters_delete_bc_hero_achievements.sql
+++ sql/custom/orangevirus/006_02_characters_delete_bc_hero_achievements.sql
@@ -1,4 +1,2 @@
 -- delete achievements of closed bc heros
 DELETE FROM `character_achievement` WHERE `achievement` IN(667, 669, 668, 670, 673, 675, 679, 672, 676, 680, 1287);
--- close bc heros
-UPDATE `creature` SET `spawnMask` = 1 WHERE `map` IN (543, 547, 542, 546, 560, 555, 554, 558, 269, 553);
